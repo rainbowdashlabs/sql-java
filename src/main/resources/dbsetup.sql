28ee20a98335512bbd541bdb6bebf1336e1d5417
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-06-03 20:16:58 +0200

    Update db setup

--- src/main/resources/dbsetup.sql
+++ src/main/resources/dbsetup.sql
@@ -4,7 +4,9 @@ create table if not exists player_coins
     -- A uuid has 36 characters.
     -- That's why we define our uuid column with a size of 36.
     -- We also say that this column should never be null.
-    uuid  char(36)         not null,
+    uuid  VARCHAR(16)       not null,
+    -- As an alternative we define the uuid as a binary with 16 bytes. This is way smaller and faster.
+    --uuid  BINARY(16)       not null,
     -- We create a coin column with a bigint. This is equal to a long in java.
     -- We also say that this column should never be null.
     -- If you just insert a new uuid into this table the coin column will be 0 by default.
